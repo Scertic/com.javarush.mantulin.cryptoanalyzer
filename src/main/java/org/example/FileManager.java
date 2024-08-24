06dcfb073f68a85ac540225734e35c27e1181ddb
Aleksei Mantulin <nilutnam@gmail.com>
2024-08-25 03:12:35 +0500

    FileManger readFile letter ю

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileManager {
-    public String readFile(String filePath) {
-        //TODO Логика чтения файла
-        return null;
+    private BufferedReader bufferedReader;
+    public String readFile(String filePath) throws IOException {
+        Path path = Path.of(filePath);
+        if (bufferedReader == null) {
+            bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+        }
+//        if (!bufferedReader.ready()) {
+//            bufferedReader.close();
+//        }
+        return bufferedReader.readLine();
     }
     public void writeFile(String content, String filePath) {
         //TODO Логика записи файла
