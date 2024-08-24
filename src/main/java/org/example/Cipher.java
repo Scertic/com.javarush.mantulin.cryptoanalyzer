a78e0c7586de8929ede001cb1a55a7b35cab41cd
Aleksei Mantulin <nilutnam@gmail.com>
2024-08-25 00:50:12 +0500

    encrypt, decrypt result StringBuilder

--- src/main/java/org/example/Cipher.java
+++ src/main/java/org/example/Cipher.java
@@ -1,16 +1,52 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Cipher {
-    private char[] alphabet;
+    private final char[] alphabet;
+    private final HashMap<Character, Integer> alphabetSet;
     public Cipher(char[] alphabet) {
         this.alphabet = alphabet;
+        this.alphabetSet = convertAlphabet(alphabet);
     }
+    public Cipher() {
+        this(new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+                'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+                'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '});
+    }
+
     public String encrypt(String text, int shift) {
-        //TODO Логика шифрования
-        return null;
+        StringBuilder result = new StringBuilder("");
+        for (int i = 0; i < text.length(); i++) {
+            if (alphabetSet.containsKey(text.toLowerCase().charAt(i))) {
+                result.append(alphabet[(alphabetSet.get(text.toLowerCase().charAt(i)) + shift) % alphabet.length]);
+            }
+        }
+        return result.toString();
     }
     public String decrypt(String encryptedText, int shift) {
-        //TODO Логика расшифровки
-        return null;
+        StringBuilder result = new StringBuilder("");
+        int findSymbol;
+        for (int i = 0; i < encryptedText.length(); i++) {
+            findSymbol = alphabetSet.get(encryptedText.toLowerCase().charAt(i));
+            if (findSymbol - shift < 0) {
+                findSymbol = alphabet.length;
+            }
+            if (alphabetSet.containsKey(encryptedText.toLowerCase().charAt(i))) {
+                result.append(alphabet[Math.abs(findSymbol - shift) % alphabet.length]);
+            }
+        }
+        return result;
+    }
+
+    private HashMap<Character, Integer> convertAlphabet(char[] alphabet) {
+        HashMap<Character, Integer> result = new HashMap<>();
+        for (int i = 0; i < alphabet.length; i++) {
+            result.put(alphabet[i],i );
+        }
+        return result;
     }
 }
