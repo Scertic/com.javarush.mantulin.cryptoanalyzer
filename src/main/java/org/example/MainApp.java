0199925cbe1fdd2d7fc6595baccdcc4f01136e7b
Aleksei Mantulin <nilutnam@gmail.com>
2024-08-25 00:51:08 +0500

    clear MainApp

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,12 +1,7 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class MainApp {
     public static void main(String[] args) {
-        Cipher cipher = new Cipher();
-        System.out.println(cipher.encrypt("проверить что он есть в вашем алфавите. Если его нет, пропускаем этот символ. ",1));
-        System.out.println(cipher.decrypt("рспгжскуэашупапоажтуэагагбщжнабмхбгкуж,ажтмкаждпаожу«арспрфтлбжнаяупуаткнгпм,а",1));
 
     }
 }
